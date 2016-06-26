4b8509b8c84450b6626b77df29b44f10e16639b0
John Morrow <CodeMorrow@yahoo.com>
2016-06-26 16:47:49 -0500

    Complete phone number formatter

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -2,5 +2,20 @@
 # returns a properly formatted phone number.
 
 def format_phone_number(phone_number_str)
-  return phone_number_str
+	#Remove non-numerical characters from the string.
+	formatted = phone_number_str.delete('^0-9')
+	
+	#Insert "(" in the front.
+	formatted.insert(0,"(")
+	
+	#Insert ")" after the third digit.
+	formatted.insert(4,")")
+	
+	#Insert " " after the area code.
+	formatted.insert(5," ")
+	
+	#Insert "-" after the sixth digit.
+	formatted.insert(9,"-")
+	
+return formatted
 end
